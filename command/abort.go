952d6455b76249be2206de937732648fb89abd10
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2021-05-13 12:26:36 +0200

    flush by closing connection

--- command/abort.go
+++ command/abort.go
@@ -8,5 +8,7 @@ import (
 // ECR can instruct the PT to abort execution of a command
 func (p *PT) Abort() error {
 	Logger.Info("ABORT")
+	p.conn.Close()
+	p.conn = nil
 	return p.SendCommand(Command{CtrlField: instr.Map["Abort"]})
 }
